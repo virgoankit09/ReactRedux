38b21421a33c14b33ca7199c036154aee24c56bc
Ankit Garg 8 <ankgarg3@publicisgroupe.net>
2018-04-20 18:02:22 +0530

    commiting workspace

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,10 @@
+export default function() {
+
+	return [
+		{ title: 'Javascript: The Good Parts'},
+		{ title: 'Harry Potter'},
+		{ title: 'The Dark tower'},
+		{ title: 'Eloquent Ruby'}
+
+	]
+}
